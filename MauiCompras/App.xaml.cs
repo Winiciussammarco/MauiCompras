23eb702ce43f0173ffd1a9a7cdebe16b1adf2077
WiniciusSammarco <winiciussaomarco@gmail.com>
2025-08-31 22:23:41 -0300

    Foi criada a pasta: helpers, models, views. Criação das classes, content pages. E alterações no código para que funcione a lista de compras, com opções de adição, salvar produto e pesquisar.

--- MauiCompras/App.xaml.cs
+++ MauiCompras/App.xaml.cs
@@ -1,12 +1,46 @@
-﻿namespace MauiCompras
+﻿using MauiCompras.Helpers;
+
+namespace MauiCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+
+                }
+
+                return _db;
+
+
+            }
+
+        }
+
+
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
         }
     }
 }
